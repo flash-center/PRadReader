9198ba95437fc236756a9d9a47bf37000a337ae0
Scott Feister <sfeister@gmail.com>
2017-11-16 16:08:46 -0600

    Updated example with comments

--- examples/loadIntermediate.py
+++ examples/loadIntermediate.py
@@ -9,11 +9,12 @@ Created by Scott Feister on Thu Nov 16 15:41:38 2017
 from pradreader.reader import loadPRR
 
 def myalgorithm(prad):
+    """ Your algorithm that runs on proton radiography data """
     pass
 
 if __name__ == "__main__":
     filename = "../test/input.txt" # Path to your intermediate .txt file
-    prad = loadPRR(filename)
+    prad = loadPRR(filename) # Load your proton radiography file
     
     print(prad.s2r_cm)
     print(prad.s2d_cm)
@@ -23,4 +24,4 @@ if __name__ == "__main__":
     print(prad.flux2D_ref) # 2D NumPy array
     print(prad.mask) # 2D NumPy array
 
-    result = myalgorithm(prad)
+    result = myalgorithm(prad) # Run your algorithm
