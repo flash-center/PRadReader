44fff816b6176b14feb0911c41175abbc39b300f
Scott Feister <sfeister@gmail.com>
2018-07-05 14:26:14 -0500

    Added another plotting function and incremented version number to 1.0a2

--- pradreader/fluxmap.py
+++ pradreader/fluxmap.py
@@ -27,7 +27,7 @@ def fluxMap(xp_cm, yp_cm, width_cm, bin_um):
         flux2D: 2D Histogram of proton flux, in units of protons/bin
         flux2D_cm2: 2D Histogram of proton flux, in units of protons/cm2
     
-    TODO: In documentation, specify which axis is X axis, which is Y axis in flux2D
+    Outputs 2D flux arrays with 'xy' indexing (axis 0 is y axis, axis 1 is x axis), not 'ij'
  
     Written by Scott Feister 2017-08-03
     """
@@ -35,7 +35,7 @@ def fluxMap(xp_cm, yp_cm, width_cm, bin_um):
     bins_cm = np.arange(-width_cm/2, width_cm/2, bin_um*1e-4) # 1D array of bin edges, in centimetres
     H, xedges_cm, yedges_cm = np.histogram2d(xp_cm, yp_cm, bins=bins_cm) # Histogram the x/y data
 
-    flux2D = H.T # Perform a transpose (flip x/y axis) on the histogram so that it plots correctly
+    flux2D = H.T # Perform a transpose (flip x/y axis) on the histogram so that it is in 'xy' indexing rather than 'ij' indexing
     
     # Convert H to proton fluence (protons/cm2)
     bin_cm2 = (bin_um*1e-4)**2 # Area of each bin
@@ -52,14 +52,15 @@ def fluxPlot(outfn, flux2D, bin_um):
     Outputs:
         Saves a flux map plot to the specified input file.
     
+    Assume 'xy' indexing (axis 0 is y axis, axis 1 is x axis), not 'ij'
+    
     Written by Scott Feister 2017-08-03
-    TODO: Check if I have flipped the X and Y axes.
     """
     fig = plt.figure(figsize=(6,5))
     ax = fig.add_subplot(111)
     
-    xmax = flux2D.shape[0] * bin_um*1.0e-4 # X/ length of detector in cm
-    ymax = flux2D.shape[1] * bin_um*1.0e-4 # Y/ width of detector in cm
+    xmax = (flux2D.shape[1] + 1) * bin_um*1.0e-4 # X/ length of detector in cm
+    ymax = (flux2D.shape[0] + 1) * bin_um*1.0e-4 # Y/ width of detector in cm
 
     cax = ax.pcolorfast([0, xmax], [0, ymax], flux2D/np.mean(flux2D), cmap='Greys')
     cbar = fig.colorbar(cax, label='Proton flux / Mean')
@@ -78,6 +79,52 @@ def fluxPlot(outfn, flux2D, bin_um):
     footString = "Mean: " + str(int(np.round(fluxMean_cm2))) + " cm$^{-2}$"
     fig.text(0.01, 0.01, footString, style='italic', horizontalalignment='left') # Lower right in figure units
 
+    plt.tight_layout(rect=[0, 0.04, 1, 0.98])
+    fig.savefig(outfn, dpi=150)
+    
+    plt.close(fig)
+    return
+
+def fluenceContrast(outfn, flux2D, flux2D_ref, bin_um, vmin=None, vmax=None):
+    """ Example plotting function for a radiograph, using matplotlib
+    Inputs:
+        outfn: String, full filename (including path) of the image file to write, e.g. "/home/myouts/myradiograph.png"
+        flux2D: Numpy array, 2D, proton flux at the detector (a.u.)
+        flux2D_ref: Numpy array, 2D, reference proton flux at the detector (a.u.)
+        bin_um: Float, size of the square edge lengths with which to divide the detector for binning
+        vmin, vmax: Max and min for the colorplot
+    Outputs:
+        Saves a fluence contrast map plot  [ (flux - flux_ref) / flux_ref ] to the specified input file.
+    
+    Assume 'xy' indexing (axis 0 is y axis, axis 1 is x axis), not 'ij'
+    
+    Written by Scott Feister 2018-07-05
+    """
+    fig = plt.figure(figsize=(6,5))
+    ax = fig.add_subplot(111)
+    
+    xmax = (flux2D.shape[1] + 1) * bin_um*1.0e-4 # X/ length of detector in cm
+    ymax = (flux2D.shape[0] + 1) * bin_um*1.0e-4 # Y/ width of detector in cm
+
+    fluence_contrast = (flux2D - flux2D_ref) / flux2D_ref
+    
+    if vmin is None:
+        vmin = np.min(fluence_contrast)
+    if vmax is None:
+        vmax = np.max(fluence_contrast)
+        
+    cax = ax.pcolorfast([0, xmax], [0, ymax], fluence_contrast, cmap='viridis', vmin=vmin, vmax=vmax)
+    cbar = fig.colorbar(cax, label='Fluence contrast (unitless)')
+    ax.set_aspect('equal')
+
+    ax.set_title('Fluence contrast, detector plane')  
+    ax.set_xticks([0, np.round(xmax, 2)])
+    ax.set_yticks([0, np.round(ymax, 2)])
+    
+    #plt.axis('off')
+    plt.xlabel('Detector X (cm)', labelpad=-10)
+    plt.ylabel('Detector Y (cm)', labelpad=-25)
+        
     plt.tight_layout(rect=[0, 0.04, 1, 0.98])
     fig.savefig(outfn, dpi=150)
     
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0a1',
+    version='1.0a2',
 
     description='Package for reading various proton radiography flux file'
                 'formats.',
